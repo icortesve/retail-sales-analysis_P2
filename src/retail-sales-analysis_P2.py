f43d15e11b2f53a3d9c166b49c30a31b86aa728c
icortesve <ivan.cortesve@gmail.com>
2026-06-03 21:08:41 -0400

    Revisión Julio 2026

--- src/retail-sales-analysis_P2.py
+++ src/retail-sales-analysis_P2.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def cargar_datos(ruta_archivo):
-    # Cargar los datos del archivo CSV utilizando NumPy
-    # Se utiliza dtype=None para identificar texto y números automáticamente
+    """Carga datos desde un archivo CSV y devuelve un DataFrame de pandas."""
+    # Cargar los datos del archivo CSV
     # encoding='utf-8' para evitar errores de lectura en caracteres especiales
-    datos = pd.read_csv(ruta_archivo)
+    datos = pd.read_csv(ruta_archivo, encoding='utf-8')
     return datos
